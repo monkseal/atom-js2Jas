eb25a83c3dad01b3b39a47cb982e17d25fdcaf59
Kevin English <kevin.english@ecoatm.com>
2017-11-13 14:19:59 -0800

    try to get plugin working

--- spec/js2jas-spec.js
+++ spec/js2jas-spec.js
@@ -9,37 +9,38 @@ describe('js2jas', () =>  {
 
   // beforeEach(() =>  {
     // atom.commands.dispatch(atom.views.getView(atom.workspace), 'js2jas:toggle-spec-file');
-    // waitsForPromise(() => 
+    // waitsForPromise(() =>
     //   activationPromise = atom.packages.activatePackage('js2jas');
-    // 
+    //
     // );
-    
+
   // });
 
   describe('when the js2jas:toggle-spec-file event is triggered', () => {
     const file = "Component.jsx";
     beforeEach(() => {
+      atom.packages.activatePackage('js2jas');
       waitsForPromise(() => atom.packages.activatePackage('js2jas'));
       waitsForPromise(() =>  atom.workspace.open(file));
 
     //   atom.workspace.open(file);
     //   const editor = atom.workspace.getActiveTextEditor();
     //   atom.commands.dispatch(atom.views.getView(editor), 'js2jas:toggle-spec-file');
-    //   return 
+    //   return
     // };
     // }) toggleFile = function(file) {
     });
 
     it('switches to spec file', () =>  {
-      
+
       runs(() => {
         const editor = atom.workspace.getActiveTextEditor();
-        expect(editor.getPath()).toContain("const editor = atom.workspace.getActiveTextEditor();")  
+        expect(editor.getPath()).toContain(file)
       });
 
       // toggleFile('app/javascript/ReactApps/components/Component.jsx');
       // atom.workspace.open(file).
-      
+
        // runs(() => expect(currentPath()).toBe(Path.join(__dirname, 'fixtures/spec/models/user_spec.rb')));
     })
   });
